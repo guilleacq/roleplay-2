ac60438e263cd802c3a35699f7a94fabaaad5a85
Guillermo Acquistapace <guillermoacq@gmail.com>
2023-09-22 12:19:20 -0300

    Fixed bug where lists were not created

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -8,20 +8,20 @@ namespace Program
     {
         static void Main(string[] args)
         {
-            IAttackItem axe = new Axe();
-            IDefenseItem helmet = new Helmet();
-            IDefenseItem shield = new Shield();
+            Axe axe = new Axe();
+            Helmet helmet = new Helmet();
+            Shield shield = new Shield();
 
-            IAttackItem sword = new Sword();
-            IDefenseItem armor = new Armor();
-            IMagicItem staff = new Staff();
+            Sword sword = new Sword();
+            Armor armor = new Armor();
+            Staff staff = new Staff();
 
             SpellsBook book = new SpellsBook();
             book.Spells = new Spell[]{ new Spell() };
 
             Wizard gandalf = new Wizard("Gandalf");
             gandalf.SpellsBook = book;
-            gandalf.AddMagicItem(staff);
+            gandalf.AddAttackItem(staff);
 
             Dwarf gimli = new Dwarf("Gimli");
             gimli.AddAttackItem(axe);
@@ -37,16 +37,16 @@ namespace Program
             santiarcher.AddAttackItem(axe);
             santiarcher.AddDefenseItem(shield);
 
-            Console.WriteLine($"Gimli has ❤️ {gimli.Health}");
-            Console.WriteLine($"Gandalf attacks Gimli with ⚔️ {gandalf.AttackValue}");
+            Console.WriteLine($"Gimli has {gimli.Health} health");
+            Console.WriteLine($"Gandalf attacks Gimli and deals {gandalf.AttackValue} damage");
 
             gimli.ReceiveAttack(gandalf.AttackValue);
 
-            Console.WriteLine($"Gimli has ❤️ {gimli.Health}");
+            Console.WriteLine($"Gimli has {gimli.Health} health");
 
             gimli.Cure();
 
-            Console.WriteLine($"Gimli has ❤️ {gimli.Health}");
+            Console.WriteLine($"Gimli has {gimli.Health} health");
         }
     }
 }
